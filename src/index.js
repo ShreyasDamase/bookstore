799c6505f071c8c948cd2f983bdcbff0e897546b
ShreyasDamase <shreyasdamase@gmail.com>
2026-02-01 19:28:28 +0530

    updated payload size

--- src/index.js
+++ src/index.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 job.start();
-app.use(express.json());
+app.use(cors());
+
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use(express.text({ limit: "25mb" }));
+
 app.use(cors());
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
